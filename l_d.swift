58bb9961f165e07e7edf05fa3c82c3668798b958
Phlippie <pbosman@retrorabbit.co.za>
2024-01-16 08:37:36 +0200

    Update l_d.swift

--- l_d.swift
+++ l_d.swift
@@ -1,10 +1,14 @@
 class DatabaseController {
+    init(connectionString: String) {
+        // ...
+    }
+
     func store(value: Any) {}
 }
 
 class ViewController {
     func onSaveData() {
-        
+        // TODO: implement
     }
     
     private let data = "some data"
