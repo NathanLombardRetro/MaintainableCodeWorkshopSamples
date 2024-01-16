50f7f06810895cdbd37e08965ceb9a33d0862d4a
NathanLombardRetro <nlombard@retrorabbit.co.za>
2024-01-16 16:00:35 +0200

    I part code

--- k_i.swift
+++ k_i.swift
@@ -1,13 +1,25 @@
 protocol Vehicle {
-    var numberOfWheels: Int { get }
     var topSpeed: Int { get }
     var numberOfDoors: Int { get }
 }
 
-class Car: Vehicle {
+protocol Aviator{
+    var maxHeight: Double {get}
+    }
+
+protocol HasWheels{
+    var numberOfWheels: Int { get }
+    }    
+
+class Car: Vehicle,HasWheels {
     var numberOfWheels: Int { 4 }
     var topSpeed: Int { 120 }
     var numberOfDoors: Int { 5 }
 }
 
-// TODO: Make Helicopter
+class Helicopter: Aviator,Vehicle, HasWheels {
+    var numberOfWheels: Int { 4 }
+    var maxHeight: Double {1289.89}
+    var numberOfDoors: Int { 4 }
+    var topSpeed: Int { 1000 }
+}
