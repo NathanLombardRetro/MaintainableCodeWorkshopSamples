cf3a8a478223f112027d70a4760de7935c84159a
NathanLombardRetro <nlombard@retrorabbit.co.za>
2024-01-16 14:00:59 +0200

    Immutable exercise

--- d_immutable.swift
+++ d_immutable.swift
@@ -1,24 +1,22 @@
-class MutablePerson {
-    var name: String = ""
+class ImmutablePerson {
+    let name: String
     
-    func setName(to newName: String) {
-        self.name = newName
+    func setName(to newName: String) -> ImmutablePerson {
+        return ImmutablePerson(name:newName)
     }
 }
 
-func checkIfPersonHasPalindromicName(_ person: MutablePerson) -> Bool {
+func checkIfPersonHasPalindromicName(_ person: ImmutablePerson) -> Bool {
     let name = person.name
-    person.setName(to: String(name.reversed()))
-    return person.name == name
+    let otherPerson=person.setName(to: String(name.reversed()))
+    return otherPerson.name == name
 }
 
 func testCheckIfPersonHasPalindromicName() {
-    let bob = MutablePerson()
-    bob.setName(to: "bob")
+    let bob = ImmutablePerson("bob")
     assert(checkIfPersonHasPalindromicName(bob) == true)
     
-    let jay = MutablePerson()
-    jay.setName(to: "jay")
+    let jay = ImmutablePerson("jay")
     assert(checkIfPersonHasPalindromicName(jay) == false)
     print(jay.name)
 }
